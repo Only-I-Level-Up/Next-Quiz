d98b1c021f7d3763e2d7fe32d5d187c7cfe53f43
lsj0202 <w1004123@gmail.com>
2024-01-15 21:37:29 +0900

    feat: add question component

--- app/question/page.tsx
+++ app/question/page.tsx
@@ -1,7 +1,22 @@
+import Question from "@/components/Question";
+import { getQuestions } from "@/utils/functions";
 import React from "react";
 
 const QuestionPage = () => {
-  return <div></div>;
+  const questions = getQuestions();
+
+  return (
+    <div className="flex flex-col spcae-y-4 md:space-y-8 p-4 lg:p-8">
+      {questions.map((question) => (
+        <Question
+          widthId={false}
+          checkEnabled={true}
+          key={question.id}
+          question={question}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default QuestionPage;
